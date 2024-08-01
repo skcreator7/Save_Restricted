74a8ea9e147f475e34e2d8bddf4146b4aebb8474
skcreator 7 <118694333+skcreator7@users.noreply.github.com>
2024-08-01 08:35:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ API_ID = int(os.environ.get("API_ID", "24379138"))
 API_HASH = os.environ.get("API_HASH", "ddaa43224616428493204bbd8dbf93b1")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://biyeyic167:newbot@cluster0.pnggzui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
